e2579c09fefbcef0289e94b0668f2af53b93bdb7
abhishekkank47 <abhishekkank5@gmail.com>
2025-04-11 12:38:34 +0530

    updated Deployed Frontend URL

--- Backend/server.js
+++ Backend/server.js
@@ -18,7 +18,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(
   cors({
-    origin : ['http://localhost:5173'],
+    origin : ['https://ecommerce-bharatgo.vercel.app'],
     methods : ['GET','POST','PUT','DELETE'],
     credentials : true
   }))
